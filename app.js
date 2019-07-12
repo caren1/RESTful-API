1056d94c9619b0e46dc2d64f8384c77bc0ab9122
Wojciech Czarnocki <30754368+caren1@users.noreply.github.com>
2019-07-12 13:30:15 +0200

    Refactored the GET, POST, DELETE methods using chained route handlers from express.js

--- app.js
+++ app.js
@@ -33,39 +33,40 @@ const Article = mongoose.model("Article", articleSchema);
 let articles = [];
 
 
-app.get("/articles", function(req, res){
-  Article.find(function(err, foundArticles){
-    if(!err){
-      res.send(foundArticles);
-    }else{
-      res.send(err);
-    }
-  });
-});
-
-app.post("/articles", function(req, res){
-
-  const newArticle = new Article ({
-    title : req.body.title,
-    content : req.body.content
-  });
-
-  newArticle.save(function(err){
-    if(!err){
-      res.send("Successfully added a new article");
-    }else{
-      res.send(err);
-    }
-  });
-});
-
-app.delete("/articles", function(req, res){
-  Article.deleteMany(function(err){
+app.route("/articles")
+  .get(function(req, res){
+    Article.find(function(err, foundArticles){
       if(!err){
-        res.send("Successfully deleted all articles.");
+        res.send(foundArticles);
       }else{
         res.send(err);
       }
-    }
-  );
-});
+    });
+  })
+
+  .post(function(req, res){
+
+    const newArticle = new Article ({
+      title : req.body.title,
+      content : req.body.content
+    });
+
+    newArticle.save(function(err){
+      if(!err){
+        res.send("Successfully added a new article");
+      }else{
+        res.send(err);
+      }
+    });
+  })
+
+  .delete(function(req, res){
+    Article.deleteMany(function(err){
+        if(!err){
+          res.send("Successfully deleted all articles.");
+        }else{
+          res.send(err);
+        }
+      }
+    );
+  });
